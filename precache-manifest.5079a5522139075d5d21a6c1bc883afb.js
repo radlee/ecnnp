f840ab215caa1904fa602559af22957804eea5ae
radlee <prof.lolito@gmail.com>
2021-05-27 16:46:56 +0200

    Updates

---
+++ precache-manifest.5079a5522139075d5d21a6c1bc883afb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283f97e22a9762757c698f2ec1e609d6",
+    "url": "/ecnnp/index.html"
+  },
+  {
+    "revision": "2f6bf9cfc3aa67f9ff9e",
+    "url": "/ecnnp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "98931894c4958a7742fa",
+    "url": "/ecnnp/static/css/main.299794c8.chunk.css"
+  },
+  {
+    "revision": "2f6bf9cfc3aa67f9ff9e",
+    "url": "/ecnnp/static/js/2.7bc29757.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ecnnp/static/js/2.7bc29757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98931894c4958a7742fa",
+    "url": "/ecnnp/static/js/main.437c8da2.chunk.js"
+  },
+  {
+    "revision": "272a5e68be2d67f5033a",
+    "url": "/ecnnp/static/js/runtime-main.9969014b.js"
+  },
+  {
+    "revision": "bb961249568787b59b33c2ac9733c101",
+    "url": "/ecnnp/static/media/bg-masthead1.bb961249.jpg"
+  },
+  {
+    "revision": "33235993e202013c2e9b39de58775628",
+    "url": "/ecnnp/static/media/bg2.33235993.jpg"
+  },
+  {
+    "revision": "af16a1a93350e0cb794c4eefd2fd04aa",
+    "url": "/ecnnp/static/media/gallery1.af16a1a9.jpg"
+  },
+  {
+    "revision": "f3253ad34dfc03ff12abdc31be96ff72",
+    "url": "/ecnnp/static/media/gallery2.f3253ad3.jpg"
+  },
+  {
+    "revision": "e708154440a35a852e6b3d2f0d0c9c77",
+    "url": "/ecnnp/static/media/gallery3.e7081544.jpg"
+  }
+]);
